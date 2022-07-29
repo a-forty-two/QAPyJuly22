37960bb35c152ce043499d5c83fdb22a5a4c3bcb
Shantanu Pandey <shantanu.pandey@live.in>
2022-07-29 16:34:55 +0530

    Package and Modules Demo

---
+++ libsdemo/banana/ostrich.py
@@ -0,0 +1,6 @@
+class Bananaconda:
+  def __init__(self):
+    self.type = "Boa"
+    self.color = "many"
+  def hiss(self):
+    return "Not Capable"
---
+++ libsdemo/program.py
@@ -0,0 +1,7 @@
+from banana.ostrich import Bananaconda
+
+snake = Bananaconda()
+
+print(dir(snake))
+
+print(snake.hiss())
